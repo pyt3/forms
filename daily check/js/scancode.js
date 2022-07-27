ed54185ab428edef5fc287ede5ee190991c09a7a
mdrpmdrp <mdrpdeveloper@gmail.com>
2022-07-28 01:20:07 +0700

    sdvdfvdf

--- daily check/js/scancode.js
+++ daily check/js/scancode.js
@@ -1,7 +1,6 @@
 
 var firestore
 window.onload = () => {
-    fetchInject(['"../js/getFirebaseAuth.js?cache=' + new Date().getTime() + '"'])
     const firebaseConfig = {
         apiKey: "AIzaSyANRS_sanVDjdunkY8z-F5UD-n3R1rgYKQ",
         authDomain: "daily-check-form.firebaseapp.com",
@@ -29,6 +28,49 @@ var liffId = {
     incubator: '1657104960-w9greg5D',
     temperature: '1657104960-92R4ZRQz',
     monitorbedside: '1657104960-p9V0QVJx'
+}
+
+async function getAuth(uid) {
+    let auth = firebase.auth()
+    let email = uid + '@dailycheck.com'
+    let password = 'dailycheck'
+    await auth.signInWithEmailAndPassword(email, password).then(function (user) {
+
+        console.log('sign in successful')
+        let user = user.user
+        console.log("🚀 ~ user", user)
+    }).catch(async function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode)
+        console.log(errorMessage)
+        console.log(email)
+        console.log(credential)
+        await auth.createUserWithEmailAndPassword(email, password).then(function (user) {
+            console.log('sign up successful')
+            let user = user.user
+            console.log("🚀 ~ user", user)
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            console.log(errorCode)
+            console.log(errorMessage)
+            console.log(email)
+            console.log(credential)
+        });
+    });
 }
 function scancode() {
     let os = liff.getOS()
