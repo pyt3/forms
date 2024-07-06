31f20cf8ed84ed279d98284ba3d64731057e0503
mdrpmdrp <mdrpdeveloper@gmail.com>
2024-07-06 22:36:45 +0700

    chore: Update service worker cache paths for improved file organization and performance

--- timesheet/service-worker.js
+++ timesheet/service-worker.js
@@ -1,27 +1,23 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('my-cache').then(cache => {
-            return cache.addAll([
-                '/forms/timesheet',
-                '/forms/timesheet/icon.png',
-                '/forms/timesheet/THSarabun.ttf',
-                '/forms/timesheet/timesheetForm.pdf',
-                '/forms/timesheet/timesheetForm no-sum.pdf',
-                '/forms/timesheet/timesheet.html'
-            ]);
-        })
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          './',
+          './timesheet.html',
+          './THSarabun.ttf',
+          './timesheetForm no-sum.pdf',
+          './timesheetForm.pdf',
+          './icon.png',
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', event => {
+  });
+  
+  self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(error => {
-                console.error('Error fetching from cache:', error);
-                return fetch(event.request);
-            })
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
     );
-});
+  });
+  
