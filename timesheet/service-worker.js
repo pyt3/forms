f68fe976e865434189ba1e807c73d7bf2a7101e3
mdrpmdrp <mdrpdeveloper@gmail.com>
2024-07-06 22:29:21 +0700

    chore: Refactor service worker registration path for improved file organization

--- timesheet/service-worker.js
+++ timesheet/service-worker.js
@@ -1,23 +1,27 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open('my-cache').then(cache => {
-        return cache.addAll([
-          '/forms/timesheet/',
-          '/forms/timesheet/icon.png',
-          '/forms/timesheet/THSarabun.ttf',
-          '/forms/timesheet/timesheetForm.pdf',
-          '/forms/timesheet/timesheetForm no-sum.pdf',
-          '/forms/timesheet/timesheet.html'
-        ]);
-      })
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/forms/timesheet',
+                '/forms/timesheet/icon.png',
+                '/forms/timesheet/THSarabun.ttf',
+                '/forms/timesheet/timesheetForm.pdf',
+                '/forms/timesheet/timesheetForm no-sum.pdf',
+                '/forms/timesheet/timesheet.html'
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', event => {
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(error => {
+                console.error('Error fetching from cache:', error);
+                return fetch(event.request);
+            })
     );
-  });
-  
+});
