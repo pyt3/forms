770bc9843b0cbbbbfeb338de22fac2dc5684ab24
mdrpmdrp <mdrpdeveloper@gmail.com>
2024-07-07 11:52:14 +0700

    chore: Refactor service worker cache paths for improved file organization and performance

--- timesheet/service-worker.js
+++ timesheet/service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('timesheet-cache').then(cache => {
         return cache.addAll([
-          './',
           './timesheet.html',
           './THSarabun.ttf',
           './timesheetForm-no-sum.pdf',
