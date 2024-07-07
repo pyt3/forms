0cbb52a6db2a40e45fffa85c83511d461e32c9e8
pyt3 <101794046+pyt3@users.noreply.github.com>
2024-07-07 11:42:23 +0700

    xfvdfgf

--- timesheet/service-worker.js
+++ timesheet/service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('timesheet-cache').then(cache => {
         return cache.addAll([
-          '/forms/timesheet/',
-          '/forms/timesheet/timesheet.html',
-          '/forms/timesheet/THSarabun.ttf',
-          '/forms/timesheet/timesheetForm-no-sum.pdf',
-          '/forms/timesheet/timesheetForm.pdf',
-          '/forms/timesheet/icon.png',
+          './',
+          './timesheet.html',
+          './THSarabun.ttf',
+          './timesheetForm-no-sum.pdf',
+          './timesheetForm.pdf',
+          './icon.png',
         ]);
       })
     );
